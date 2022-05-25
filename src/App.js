ce2271e034b1a6cb54ca29b4449ae6becb59cfcf
Hailey Wu <haileyyiyu@gmail.com>
2022-05-24 20:13:58 -0700

    added css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import CardList from "./CardList";
 import {robots} from './robots';
 import SearchBox from "./SearchBox";
+import './App.css';
+import 'tachyons';
 
 
 class App extends React.Component{
@@ -25,7 +27,7 @@ class App extends React.Component{
         )
         return (
         <div className="tc">
-            <h1>RoboFriends</h1>
+            <h1 className="f1">RoboFriends</h1>
             <SearchBox searchChange={this.onSearchChange}/>
             <CardList robots={filteredRobots}/>
         </div>
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -4,7 +4,7 @@ function SearchBox({searchChange}){
   return (
       <div className="pa2">
         <input type='search' 
-        className="pa3 ba b--pink bg-washed-red"
+        className="pa3 ba b--dark-pink bg-washed-red"
         placeholder="search robots" 
         onChange = {searchChange}/>
       </div>
