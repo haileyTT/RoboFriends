4e27a07262c00e274d6af14a32666d2027deb086
Hailey Wu <haileyyiyu@gmail.com>
2022-05-25 15:24:05 -0700

    JsonPlaceHolder

---
+++ src/Containers/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import CardList from "../Components/CardList";
+import SearchBox from "../Components/SearchBox";
+import './App.css';
+import 'tachyons';
+import Scroll from '../Components/Scroll'
+
+
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            robots: [],
+            searchfield: "",
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            return response.json();
+        })
+        .then(users => {
+            this.setState({robots: users})
+        }); 
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+    render(){
+        const filteredRobots = this.state.robots.filter(
+            robot => {
+                return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+            }
+        )
+        if (this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        } else {
+            return (
+            <div className="tc">
+                <h1 className="f2">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                   <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+            );
+        }
+    }
+}
+
+export default App;
